59fae6c85e8e07a306753be5b67189aa30e3cf38
Kingsley <asromafanisme@gmail.com>
2018-11-11 15:11:46 +0800

    main UI for the remote controller

--- app/src/main/java/com/mkhoi/chromeremotecontroller/MainActivity.kt
+++ app/src/main/java/com/mkhoi/chromeremotecontroller/MainActivity.kt
@@ -16,14 +16,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setSupportActionBar(toolbar)
 
         val data = HashMap<String, String>()
         data["receiverTokenId"] = "APA91bFBTL0rg4Tw9dxRvwgWG94NHXSkg-7_Aj-E6WSSj5hXT3Z5O7E9PN6CRImtQdUaLNFqlGilI2HqslUkW7q_IK3qzewlfo1BT3pw8WrqdEMGnCivc48ynTv_FiIpYqggLvBcdcNS"
         data["command"] = "Test"
         data["optionalParameter"] = ""
 
-        fab.setOnClickListener { _ ->
+        /*fab.setOnClickListener { _ ->
             FirebaseFunctions.getInstance()
                     .getHttpsCallable("sendMessage")
                     .call(data)
@@ -33,22 +32,6 @@ class MainActivity : AppCompatActivity() {
                     .addOnSuccessListener {
                         Log.d("Firebase", "Done")
                     }
-        }
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
-        }
+        }*/
     }
 }
